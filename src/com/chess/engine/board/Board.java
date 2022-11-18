8554ac5db769e74909a78efac82d7a278bdecf08
LDTSon <105378016+LDTSon@users.noreply.github.com>
2022-11-18 16:41:22 +0700

    commit King

--- src/com/chess/engine/board/Board.java
+++ src/com/chess/engine/board/Board.java
@@ -1,8 +1,164 @@
 package com.chess.engine.board;
 
+import com.chess.engine.Alliance;
+import com.chess.engine.pieces.*;
+import com.chess.engine.player.BlackPlayer;
+import com.chess.engine.player.Player;
+import com.chess.engine.player.WhitePlayer;
+
+
+import java.util.*;
+import java.util.stream.Stream;
+
 public class Board {
 
-    public Tile getTile(final int tilecoCoordinate) {
-        return null;
+    private final List<Tile> gameBoard;
+    private final Collection<Piece> blackPieces;
+    private final Collection<Piece> whitePieces;
+    private final WhitePlayer whitePlayer;
+    private final BlackPlayer blackPlayer;
+    private final Player currentPlayer;
+
+    private Board(final Builder builder) {
+        this.gameBoard = createGameBoard(builder);
+        this.blackPieces = calculateActivePiece(this.gameBoard, Alliance.BLACK);
+        this.whitePieces = calculateActivePiece(this.gameBoard, Alliance.WHITE);
+        final Collection<Move> whiteStandardLegalMove = calculateLegalMove(this.whitePieces);
+        final Collection<Move> blackStandardLegalMove = calculateLegalMove(this.blackPieces);
+        this.whitePlayer = new WhitePlayer(this, whiteStandardLegalMove, blackStandardLegalMove);
+        this.blackPlayer = new BlackPlayer(this, blackStandardLegalMove, whiteStandardLegalMove);
+        this.currentPlayer = builder.nextMoveMaker.choosePlayer(this.whitePlayer, this.blackPlayer);
+    }
+
+    public String toString(){
+        final StringBuilder builder = new StringBuilder();
+        for (int i=0; i<BoardUtils.NUM_TILES; i++) {
+            final String tileText = this.gameBoard.get(i).toString();
+            builder.append(String.format("%3s", tileText));
+            if ((i+1) %  BoardUtils.NUM_TILES_PER_ROW == 0) {
+                builder.append("\n");
+            }
+        }
+        return builder.toString();
+    }
+
+    public Player whitePlayer() { return this.whitePlayer; }
+
+    public Player blackPlayer() {
+        return this.blackPlayer;
+    }
+
+    public Player currentPlayer() { return this.currentPlayer; }
+
+    public Collection<Piece> getBlackPieces(){
+        return this.blackPieces;
+    }
+
+    public Collection<Piece> getWhitePieces(){
+        return this.whitePieces;
+    }
+
+    private Collection<Move> calculateLegalMove(final Collection<Piece> pieces) {
+        final List<Move> legalMoves = new ArrayList<>();
+        for (final Piece piece : pieces){
+            legalMoves.addAll(piece.calculateLegalMove(this));
+        }
+        return legalMoves;
+    }
+
+    private static Collection<Piece> calculateActivePiece(final List<Tile> gameBoard, final Alliance alliance) {
+        final List<Piece> activePieces = new ArrayList<>();
+        for(final Tile tile : gameBoard) {
+            if (tile.isTileOccupied()){
+                final Piece piece = tile.getPiece();
+                if (piece.getPieceAlliance() == alliance) {
+                    activePieces.add(piece);
+                }
+            }
+        }
+        return activePieces;
+    }
+    public Tile getTile(final int tileCoordinate) {
+        return gameBoard.get(tileCoordinate);
+    }
+
+    private static List<Tile> createGameBoard(final Builder builder) {
+        final Tile[] tiles = new Tile[BoardUtils.NUM_TILES];
+        for (int i=0; i<BoardUtils.NUM_TILES; i++) {
+            tiles[i] = Tile.createTile(i, builder.boardConfig.get(i));
+        }
+     return Arrays.asList(tiles);
+    }
+
+    public static Board createStandardBoard(){ //thiết lập vị trí quân ban đầu
+        final Builder builder = new Builder();
+        //Bên đen
+        builder.setPiece(new Rook(0, Alliance.BLACK));
+        builder.setPiece(new Knight(1, Alliance.BLACK));
+        builder.setPiece(new Bishop(2, Alliance.BLACK));
+        builder.setPiece(new Queen(3, Alliance.BLACK));
+        builder.setPiece(new King(4, Alliance.BLACK));
+        builder.setPiece(new Bishop(5, Alliance.BLACK));
+        builder.setPiece(new Knight(6, Alliance.BLACK));
+        builder.setPiece(new Rook(7, Alliance.BLACK));
+        builder.setPiece(new Pawn(8, Alliance.BLACK));
+        builder.setPiece(new Pawn(9, Alliance.BLACK));
+        builder.setPiece(new Pawn(10, Alliance.BLACK));
+        builder.setPiece(new Pawn(11, Alliance.BLACK));
+        builder.setPiece(new Pawn(12, Alliance.BLACK));
+        builder.setPiece(new Pawn(13, Alliance.BLACK));
+        builder.setPiece(new Pawn(14, Alliance.BLACK));
+        builder.setPiece(new Pawn(15, Alliance.BLACK));
+        //Bên trắng
+        builder.setPiece(new Rook(63, Alliance.WHITE));
+        builder.setPiece(new Knight(62, Alliance.WHITE));
+        builder.setPiece(new Bishop(61, Alliance.WHITE));
+        builder.setPiece(new Queen(60, Alliance.WHITE));
+        builder.setPiece(new King(59, Alliance.WHITE));
+        builder.setPiece(new Bishop(58, Alliance.WHITE));
+        builder.setPiece(new Knight(57, Alliance.WHITE));
+        builder.setPiece(new Rook(56, Alliance.WHITE));
+        builder.setPiece(new Pawn(55, Alliance.WHITE));
+        builder.setPiece(new Pawn(54, Alliance.WHITE));
+        builder.setPiece(new Pawn(53, Alliance.WHITE));
+        builder.setPiece(new Pawn(52, Alliance.WHITE));
+        builder.setPiece(new Pawn(51, Alliance.WHITE));
+        builder.setPiece(new Pawn(50, Alliance.WHITE));
+        builder.setPiece(new Pawn(49, Alliance.WHITE));
+        builder.setPiece(new Pawn(48, Alliance.WHITE));
+        //Lượt trắng đi trước
+        builder.setMoveMaker(Alliance.WHITE);
+
+        return builder.build();
+    }
+
+    public Iterable<Move> getAllLegalMoves() {
+        return (Iterable<Move>) Stream.concat(this.whitePlayer.getLegalMoves().stream(), this.blackPlayer.getLegalMoves().stream());
+    }
+    public static class Builder {
+        Map<Integer, Piece> boardConfig;
+        Alliance nextMoveMaker;
+
+        public Builder(){
+            this.boardConfig = new HashMap<>();
+        }
+
+        public Builder setPiece(final Piece piece){
+            this.boardConfig.put(piece.getPiecePosition(), piece);
+            return this;
+        }
+
+        public Builder setMoveMaker(final Alliance nextMoveMaker) {
+            this.nextMoveMaker = nextMoveMaker;
+            return this;
+        }
+
+        public Board build(){
+            return new Board(this);
+        }
+
+        public void setEnPassantPawn(Pawn enPassantPawn) {
+            this.enPassantPawn = 
+        }
     }
 }
---
+++ src/com/chess/engine/pieces/King.java
@@ -0,0 +1,64 @@
+package com.chess.engine.pieces;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class King extends Piece{
+
+    private final static int[] CANDIDATE_MOVE_COORDINATES = {-9, -8, -7, -1, 1, 7, 8, 9}; //tất cả nước đi của quân vua theo tọa độ
+
+    public King(final int piecePosition, final Alliance pieceAlliance) {
+        super(PieceType.KING, piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMove(final Board board) {//tìm nước đi hợp lệ
+
+        final List<Move> legalMoves = new ArrayList<>();//danh sách các nước có thể đi
+
+        for(final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATES) { //duyệt tất cả nước đi
+            final int candidateDestinationCoordinate = this.piecePosition + currentCandidateOffset; //tọa độ mới tất cả các trường hợp đi quân
+            if(IsFirstColumnExclusion(this.piecePosition, currentCandidateOffset) || 			//nếu tọa độ ở cột 1
+                    isEighthColumnExclusion(this.piecePosition, currentCandidateOffset)) {		//hoặc nếu tọa độ ở cột 8
+                continue;																		//không xét 1 số nước nữa (do ra khỏi bàn cờ)
+            }
+            if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {					//nếu tọa độ đó nằm trong bàn cờ
+                final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);            //tile chứa tọa độ dự kiến
+                if(!candidateDestinationTile.isTileOccupied()) {												//nếu tile không bị chiếm
+                    legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));	//nước đi đến tile đó là hợp lệ
+                } else {
+                    final Piece pieceAtDestination = candidateDestinationTile.getPiece();						//lấy quân chiếm ô đó
+                    final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();						//lấy thuộc tính Màu quân chiếm ô đó
+                    if(this.pieceAlliance != pieceAlliance) {													//nếu màu quân chiếm khác màu quân đi nước đó
+                        legalMoves.add(new Move.AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));//nước đi đến ô đó là hợp lệ
+                    }
+                }
+            }
+        }
+        return legalMoves;
+    }
+
+    @Override
+    public King movePiece(Move move) {
+        return new King(move.getDestinationCoordinate(), move.getmovedPiece().getPieceAlliance());
+    }
+
+    public String toString() {
+        return PieceType.KING.toString();
+    }
+    private static boolean IsFirstColumnExclusion(final int currentPosition, final int candidateOffset) {
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -9 || candidateOffset == 7 || candidateOffset == -1);
+    } //trả về 1 nếu vị trí hiện tại thuộc cột 1 và nước đi dự tính {-9, -1, 7}
+
+    private static boolean isEighthColumnExclusion(final int currentPosition, final int candidateOffset) {
+        return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == -7 || candidateOffset == 1 || candidateOffset == 9);
+    } //trả về 1 nếu vị trí hiện tại thuộc cột 8 và nước đi dự tính {-7, 1, 9}
+
+}
